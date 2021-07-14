521952534aea054a680249f5c0aebceebebdab50
sidhantgumber <gumbersidhant@gmail.com>
2021-07-14 23:32:02 +0530

    Added the first script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Thrusters Active");
+        }
+    
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Left");
+
+
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("right");
+        }
+    
+    
+    
+    }
+}
