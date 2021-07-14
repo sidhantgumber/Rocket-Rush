eb776a58bf051d8d838f6f2eb497195257e05186
sidhantgumber <gumbersidhant@gmail.com>
2021-07-15 00:25:52 +0530

    Bug fixes: gravity drag obstacles constraints

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Rigidbody rb;
+    public float thrustValue = 100f;
+    public float rotationThrustValue = 100f;
+   
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         ProcessThrust();
@@ -21,7 +26,8 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Thrusters Active");
+            // Debug.Log("Thrusters Active");
+            rb.AddRelativeForce(Vector3.up * thrustValue * Time.deltaTime);
         }
     
     }
@@ -30,17 +36,24 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Left");
-
+            ApplyRotation(rotationThrustValue);
 
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("right");
+            ApplyRotation(-rotationThrustValue);
+
         }
-    
-    
-    
+
+
+
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;    
     }
 }
