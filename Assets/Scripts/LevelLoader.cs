6ddd2cb9f1b895f1e4a0e0963b5b8c6729cf5525
sidhantgumber <gumbersidhant@gmail.com>
2021-07-23 19:42:01 +0530

    Created instructions menu + Polished UI

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -7,11 +7,42 @@ public class LevelLoader : MonoBehaviour
 
    public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Level 1");
     
     }
    public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void LoadNextScreen()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+
+    }
+
+   public void LoadPreviousScreen()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int previousSceneIndex = currentSceneIndex - 1;
+        if (previousSceneIndex<0)
+        {
+            previousSceneIndex = 0;
+
+        }
+        SceneManager.LoadScene(previousSceneIndex);
+
+    }
+
+    public void LoadInstructionsMenu()
+    {
+        SceneManager.LoadScene("Instructions");
+    
     }
 }
